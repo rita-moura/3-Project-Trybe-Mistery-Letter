d31b8cbd3e1fb278360dcbdeca2952e1852958e0
Rita Moura <ritamoura520@gmail.com>
2023-07-22 19:08:23 -0300

    Feat: adiciona classes aleatorias ao paragrafo.

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
 const buttonCreateLetter = document.getElementById('criar-carta');
 const generatedLetter = document.getElementById('carta-gerada');
 
+const groups = [
+  ['newspaper', 'magazine1', 'magazine2'],
+  ['medium', 'big', 'reallybig'],
+  ['rotateleft', 'rotateright'],
+  ['skewleft', 'skewright'],
+];
+
+function assignRandomClasses() {
+  const words = generatedLetter.getElementsByTagName('span');
+  Array.from(words).forEach((word) => {
+    const availableGroups = groups.map((group) => [...group]);
+    const assignedClasses = [];
+    availableGroups.forEach((group) => {
+      if (group.length === 0) return;
+      const randomIndex = Math.floor(Math.random() * group.length);
+      const randomClass = group.splice(randomIndex, 1)[0];
+      assignedClasses.push(randomClass);
+    });
+    word.classList.add(...assignedClasses);
+  });
+}
+
 const createLetter = () => {
   const letterText = document.getElementById('carta-texto').value;
 
@@ -10,6 +32,8 @@ const createLetter = () => {
     const words = letterText.split(' ');
     const letterWithSpans = words.map((word) => `<span>${word}</span>`).join(' ');
     generatedLetter.innerHTML = letterWithSpans;
+
+    assignRandomClasses();
   }
 };
 
